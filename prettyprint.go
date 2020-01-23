858558985f2c75869ff350bbc4763f0c3beba855
work <26752232+githubsands@users.noreply.github.com>
2020-01-22 17:05:48 -0700

    Print the line where the method print is now called

--- prettyprint.go
+++ prettyprint.go
@@ -3,11 +3,12 @@ package prettyprint
 import (
 	"fmt"
 	"reflect"
+	"runtime"
 )
 
 const (
-	printed        = "%v: Type: %T, Value: %v\n"
-	printedDynamic = "%v: Type: %v, Value: %v\n"
+	printed        = "%v: Type: %T, Value: %v, Line: %v\n"
+	printedDynamic = "%v: Type: %v, Value: %v, Line: %v\n"
 )
 
 type Printer struct {
@@ -21,11 +22,13 @@ func NewPrinter() *Printer {
 func (p *Printer) Print(i interface{}) {
 	defer p.count()
 
+	_, _, line, _ := runtime.Caller(1)
+
 	switch v := i.(type) {
 	case int, int8, int16, int64, uint, uint16, uint32, uintptr, string, bool, byte, rune, float32, float64:
-		fmt.Printf(printed, p.counter, v, v)
+		fmt.Printf(printed, p.counter, v, v, line)
 	default:
-		fmt.Printf(printedDynamic, p.counter, reflect.TypeOf(i).String(), reflect.ValueOf(i))
+		fmt.Printf(printedDynamic, p.counter, reflect.TypeOf(i).String(), reflect.ValueOf(i), line)
 	}
 
 	return
